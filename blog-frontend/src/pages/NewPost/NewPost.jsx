feaa4ad8ecb9183fe91590a8a2b07b87a9df5046
César Guerra <warderer@gmail.com>
2025-07-07 21:02:57 -0600

    ✨ Add Yup validations to NewPost form

--- blog-frontend/src/pages/NewPost/NewPost.jsx
+++ blog-frontend/src/pages/NewPost/NewPost.jsx
@@ -1,8 +1,11 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { postSchema } from '../../schemas/Post';
 import './NewPost.css'
 
 const NewPost = () => {
   const { register, handleSubmit, reset, formState: { errors, isSubmitting } } = useForm({
+    resolver: yupResolver(postSchema),
     defaultValues: {
         title: '',
         content: '',
@@ -52,11 +55,11 @@ const NewPost = () => {
           <input
             type='text'
             id='title'
-            className='form-input' // TODO: Añadir clase de error si hay errores
+            className={`form-input ${errors.title ? 'error' : ''}`}
             placeholder='Ingresa un título para tu post'
             {...register('title')}
           />
-          {/* TODO: Mostrar mensaje de error */}
+          <span className='error-message'>{errors.title?.message}</span>
         </div>
 
         <div className='form-group'>
@@ -66,11 +69,11 @@ const NewPost = () => {
           <textarea
             id='content'
             rows='6'
-            className='form-textarea' // TODO: Añadir clase de error si hay
+            className={`form-textarea ${errors.title ? 'error' : ''}`}
             placeholder='Escribe el contenido de tu post aquí...'
             {...register('content')}
           />
-          {/* TODO: Mostrar mensaje de error */}
+          <span className='error-message'>{errors.content?.message}</span>
         </div>
 
         <div className='form-row'>
@@ -80,7 +83,7 @@ const NewPost = () => {
             </label>
             <select
               id='category'
-              className='form-select ' // TODO: Añadir clase de error si hay
+              className={`form-select ${errors.title ? 'error' : ''}`}
               {...register('category')}
             >
               <option value=''>Selecciona una categoría</option>
@@ -90,7 +93,7 @@ const NewPost = () => {
                 </option>
               ))}
             </select>
-            {/* TODO: Mostrar mensaje de error */}
+            <span className='error-message'>{errors.category?.message}</span>
           </div>
 
           <div className='form-group'>
@@ -100,11 +103,11 @@ const NewPost = () => {
             <input
               type='text'
               id='author'
-              className='form-input ' // TODO: Añadir clase de error si hay
+              className={`form-input ${errors.title ? 'error' : ''}`}
               placeholder='Tu nombre'
               {...register('author')}
             />
-            {/* TODO: Mostrar mensaje de error */}
+            <span className='error-message'>{errors.author?.message}</span>
           </div>
         </div>
 
@@ -115,17 +118,17 @@ const NewPost = () => {
           <input
             type='datetime-local'
             id='publishDate'
-            className='form-input ' // TODO: Añadir clase de error si hay
+            className={`form-input ${errors.title ? 'error' : ''}`}
             {...register('publishDate')}
           />
-          {/* TODO: Mostrar mensaje de error */}
+          <span className='error-message'>{errors.publishDate?.message}</span>
         </div>
 
         <div className='form-actions'>
           <button
             type='button'
             className='btn btn-secondary'
-            onClick={() => { reset() }} // TODO: Añadir reset del formulario
+            onClick={() => { reset() }}
           >
             Limpiar
           </button>
