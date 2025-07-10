4e136e89e92d712bbeea1c2eab81f813a5e7f6aa
César Guerra <warderer@gmail.com>
2025-07-09 19:40:05 -0600

    ✨ Add Header component

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import BlogRoutes from './routes/BlogRoutes'
+import Header from './components/Header/Header'
 import './App.css'
 
 function App () {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <div className='app'>
           <main className='app-main'>
             <div className='container'>
