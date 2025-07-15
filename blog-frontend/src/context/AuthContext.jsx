a3a0e2c0d7e67674e70cdf097834b9b3368d6738
César Guerra <warderer@gmail.com>
2025-07-14 20:51:35 -0600

    ✨ Update context with React 19 changes

--- blog-frontend/src/context/AuthContext.jsx
+++ blog-frontend/src/context/AuthContext.jsx
@@ -40,9 +40,9 @@ function AuthProvider ({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={data}>
+    <AuthContext value={data}>
       {children}
-    </AuthContext.Provider>
+    </AuthContext>
   )
 }
 
--- blog-frontend/src/hooks/useAuth.jsx
+++ blog-frontend/src/hooks/useAuth.jsx
@@ -1,9 +1,9 @@
-import { useContext } from 'react'
+import { use } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 // 3. Crear un hook para usar el contexto de autenticación
 export const useAuthContext = () => {
-  const context = useContext(AuthContext)
+  const context = use(AuthContext)
 
   if (!context) {
     throw new Error('useAuthContext must be used within an AuthProvider')
