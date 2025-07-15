894ee553b6c1864f67617a65dca05dbe691245d6
César Guerra <warderer@gmail.com>
2025-07-14 19:44:23 -0600

    ✨ Add useAuthContext custoom hook

---
+++ blog-frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+
+// 3. Crear un hook para usar el contexto de autenticación
+export const useAuthContext = () => {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuthContext must be used within an AuthProvider')
+  }
+
+  return context
+}
